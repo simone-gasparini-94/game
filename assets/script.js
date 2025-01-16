052dadee11f6e820a140756a1397655f572bdef5
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-16 19:16:43 +0100

    Add movement with arrowkeys

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,47 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height / 2;
 
+
 function drawBall() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "hwb(0 100% 4%)";
     ctx.fill();
     ctx.closePath();
 }
-setInterval(drawBall, 10);
+
+function moveUp() {
+    y -= 4;
+    drawBall();
+}
+
+function moveDown() {
+    y += 4;
+    drawBall();
+}
+
+function moveLeft() {
+    x -= 4;
+    drawBall();
+}
+
+function moveRight() {
+    x += 4;
+    drawBall();
+}
+
 
 drawBall();
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+        moveUp();
+    } else if (event.key === "ArrowDown") {
+        moveDown();
+    } else if (event.key === "ArrowLeft") {
+        moveLeft();
+    } else if (event.key === "ArrowRight") {
+        moveRight();
+    }
+})
