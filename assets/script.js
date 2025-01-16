e8aeb1b7ce341cd6c348d16b2aed04fcbd13ed06
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-16 18:54:55 +0100

    Add function and refresher

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(230, 310, 20, 20);
-ctx.fillStyle = "hwb(0 100% 4%)";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+    ctx.fillStyle = "hwb(0 100% 4%)";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(drawBall, 10);
+
+drawBall();
